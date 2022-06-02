bb06d07ad9092dcec1154734d1bad3667b700b7e
limyeonsang <limquo@gmail.com>
2022-06-02 09:58:01 +0900

    main paging

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as LandingContainer } from "./Landing"
 export { default as SigninContainer } from "./Landing/Sign/Signin"
 export { default as SignupContainer } from "./Landing/Sign/Signup"
+export { default as MainContainer } from "./Main"
