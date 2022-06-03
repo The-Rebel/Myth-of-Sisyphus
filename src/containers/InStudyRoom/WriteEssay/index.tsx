7d9f6390bc720c40af35d71e381def3be650e50d
limyeonsang <limquo@gmail.com>
2022-06-03 13:40:57 +0900

    Add: mantine text editor

--- src/containers/InStudyRoom/WriteEssay/index.tsx
+++ src/containers/InStudyRoom/WriteEssay/index.tsx
@@ -1,5 +1,25 @@
+import * as S from "./styles"
+import { RichTextEditor } from '@mantine/rte';
+import { useEffect, useState } from "react";
+
+const initialValue ='<h1 class="ql-align-center">제목</h1><p>내용</p>'
+
 const WriteEssayContainer = () => {
-    return <div>dfsklfnskln</div>
+    const [text, setText] = useState<string>(initialValue)
+
+    return (
+        <S.WriteEssayContainer>
+            <input placeholder="제목을 입력하세요"></input>
+            <RichTextEditor value={text} onChange={setText}
+            controls={[
+                ['bold', 'italic', 'underline', 'strike'],
+                ['h1', 'h2', 'h3', 'h4'],
+                ['unorderedList', 'orderedList'],
+                ['blockquote', 'link'],
+                ['alignLeft', 'alignCenter', 'alignRight']
+            ]}/>
+        </S.WriteEssayContainer>
+    )
 }
 
 export default WriteEssayContainer;
---
+++ src/containers/InStudyRoom/WriteEssay/styles.ts
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const WriteEssayContainer = styled.section`
+    margin-top: 60px;
+`
