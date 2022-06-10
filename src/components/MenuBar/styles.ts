f1ff667d2c8099f2060c76fa2da966f55a0ec7ed
limyeonsang <limquo@gmail.com>
2022-06-11 00:12:43 +0900

    container

--- src/components/MenuBar/styles.ts
+++ src/components/MenuBar/styles.ts
@@ -6,3 +6,20 @@ export const MenuBarArrow = styled.div`
     bottom: 50px;
     cursor: pointer;
 ` 
+
+export const MenuBarContainer = styled.section`
+    position: fixed;
+    bottom: 0px;
+
+    width: 100%;
+    height: 593px;
+    background-color: #1B1B1B;
+    z-index: 3;
+`
+
+export const CloseArrow = styled.div`
+    position: absolute;
+    left: 50%;
+    top: 30px;
+    cursor: pointer;
+` 
